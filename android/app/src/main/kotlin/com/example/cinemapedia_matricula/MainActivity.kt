4cd0bd1384380797db0bb8f046deea6ed4d47bb0
Ana Karen Crisanto Reyes <pananaxdana@gmail.com>
2025-11-18 23:42:19 -0600

    Agregando nuevos archivos a Cinemapedia y subiendo por primera vez mis archivos

---
+++ android/app/src/main/kotlin/com/example/cinemapedia_matricula/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cinemapedia_220094
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -0,0 +1,84 @@
+import 'dart:async';
+
+import 'package:cinemapedia_220094/domain/entities/movie.dart';
+import 'package:flutter/material.dart';
+
+typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
+
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
+  final SearchMoviesCallback searchMovies;
+  List<Movie> initialMovies;
+
+  StreamController<List<Movie>> debouncedMovies = StreamController.broadcast();
+  StreamController<bool> isLoadingStrean = StreamController.broadcast();
+
+  Timer? _debounceTimer;
+
+  SearchMovieDelegate({required this.searchMovies, required this.initialMovies})
+    : super(searchFieldLabel: 'Buscar películas');
+
+  void clearStreams() {
+    debouncedMovies.close();
+  }
+
+  void _onQueryChanged(String query) {
+    isLoadingStrean.add(true);
+
+    if (_debounceTimer?.isActive ?? false) _debounceTimer!.cancel();
+
+    _debounceTimer = Timer(const Duration(milliseconds: 500), () async {
+      final movies = await searchMovies(query);
+      initialMovies = movies;
+      debouncedMovies.add(movies);
+      isLoadingStrean.add(false);
+    });
+  }
+
+  Widget buildResultsAndSuggestions() {
+    return StreamBuilder(
+      initialData: initialMovies,
+      stream: debouncedMovies.stream,
+      builder: (context, snapshot) {
+        final movies = snapshot.data ?? [];
+
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (context, index) => _MovieItem(
+            movie: movies[index],
+            onMovieSelected: (context, movie) {
+              clearStreams();
+              close(context, context);
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    // TODO: implement buildActions
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    throw UnimplementedError();
+  }
+}
+
+_MovieItem({required Movie movie, required Null Function(dynamic context, dynamic movie) onMovieSelected}) {
+}
